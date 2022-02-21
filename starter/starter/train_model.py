c0404f1226ac6eaff39bcccca258118f8d6890b6
Siphu Langeni <szlangeni@gmail.com>
2022-02-21 05:31:08 -0500

    Model training

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,11 +3,17 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+from joblib import dump
+from ml.data import process_data
+from ml.model import train_model
+
 
 # Add code to load in the data.
+data = pd.read_csv('../data/census_clean.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 cat_features = [
     "workclass",
@@ -24,5 +30,17 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder_test, lb_test = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+dump(model, '../model/rfc_model.joblib')
+dump(encoder, '../model/encoder.joblib')
+dump(lb, '../model/lb.joblib')
